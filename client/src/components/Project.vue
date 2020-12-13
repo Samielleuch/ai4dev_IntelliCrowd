9f2a0d03e93399ef70516da1bfff9f40fa0c0947
Sami <sami.elleuch@insat.u-carthage.tn>
2020-12-13 19:56:53 +0100

    compaign specs ui improvements

--- client/src/components/Project.vue
+++ client/src/components/Project.vue
@@ -1,277 +1,254 @@
 <template>
   <div>
-    <v-row>
-      <v-col md="1"></v-col>
-      <v-col>
-        <v-hover v-slot:default="{ hover }">
-          <v-card :elevation="hover ? 16 : 2" height="500" width="500"
-            ><v-img
-              src="../assets/image.jpg"
-              height="500"
-              width="500"
-            ></v-img></v-card></v-hover
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="7" class="pl-12" align="center" justify="center">
+        <v-img src="../assets/image.jpg" contain max-width="900"></v-img
       ></v-col>
-      <v-col
-        ><v-hover v-slot:default="{ hover }">
-          <v-card
-            :elevation="hover ? 16 : 2"
-            height="500"
-            width="500"
-            color="#012116"
-            ><v-toolbar color="#977C1E" height="90px">
-              <v-toolbar-title style="padding-right: 20px ; padding-left : 95px"
-                >Funds :
-              </v-toolbar-title>
-              <h2>2000 / 10000 DT</h2>
-            </v-toolbar>
-            <br />
-            <br />
-            <v-toolbar color="#977C1E" height="90px">
-              <v-toolbar-title style="padding-right: 20px ; padding-left : 95px"
-                >Number of backers :
-              </v-toolbar-title>
-              <h2>50</h2>
-            </v-toolbar>
-            <br />
-            <br />
-            <v-toolbar color="#977C1E" height="90px">
-              <v-toolbar-title style="padding-right: 20px ; padding-left : 95px"
-                >Days to go :
-              </v-toolbar-title>
-              <h2>125</h2>
-            </v-toolbar>
-            <br />
-            <br />
-            <v-row
-              ><v-col align="center">
-                <v-btn
-                  rounded
-                  width="250px"
-                  height="60px"
-                  class="btn"
-                  color="#977C1E"
-                  @click="validate"
-                >
-                  Fund it !
-                </v-btn>
-              </v-col>
-            </v-row></v-card
-          ></v-hover
-        ></v-col
-      ></v-row
-    >
-    <br />
-    <v-row>
-      <v-col md="1"></v-col
-      ><v-col
-        ><v-hover v-slot:default="{ hover }">
-          <v-card
-            style=" background-color: #977C1E"
-            :elevation="hover ? 16 : 2"
-            height="200"
-            width="1130"
-            color="#012116"
-            ><h2 style="padding-top: 30px ; padding-left: 30px">
-              We are developing a website that helps hotels reach more clients
-              through virtual visits.It will revolutionize the tourism approach
-              in Tunisia.It must be done by the end of august ...
-            </h2>
-            <v-row style="padding-top: 10px "
-              ><v-col align="center">
-                <v-btn
-                  style="color: white"
-                  rounded
-                  width="180px"
-                  class="btn black"
-                >
-                  Read More
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-hover></v-col
-      ></v-row
-    >
-    <br />
-    <br />
-    <br />
-    <v-row>
-      <v-col md="1"></v-col
-      ><v-col
-        ><v-hover v-slot:default="{ hover }">
-          <v-card
-            style=" background-color: #012116"
-            :elevation="hover ? 16 : 2"
-            height="500"
-            width="1130"
-            color="#012116"
+      <v-col cols="12" md="5">
+        <v-row align="center">
+          <v-col align="center">
+            <v-card height="430" width="450" class=" rounded s">
+              <v-row align="center">
+                <v-col align="center">
+                  <v-row class="mt-12">
+                    <v-col align="center">
+                      <h1 style="color: #806749" class="toptext ">
+                        Funds :
+                        <span style="color: black; margin-left:8px"
+                          >2000/10000 DT</span
+                        >
+                      </h1>
+                    </v-col>
+                  </v-row>
+                  <hr class="divider" />
+                  <v-row>
+                    <v-col align="center">
+                      <h1 style="color: #806749" class="toptext ">
+                        Number Of Backers :
+                        <span style="color: black; margin-left:8px">50</span>
+                      </h1>
+                    </v-col>
+                  </v-row>
+                  <hr class="divider" />
+
+                  <v-row>
+                    <v-col align="center">
+                      <h1 style="color: #806749" class="toptext ">
+                        Days to Go :
+                        <span style="color: black; margin-left:8px">125</span>
+                      </h1>
+                    </v-col>
+                  </v-row>
+                  <hr class="divider" />
+
+                  <v-row
+                    ><v-col align="center">
+                      <v-btn
+                        rounded
+                        width="250px"
+                        dark
+                        height="40px"
+                        class=" my-6 toptext"
+                        color="#0a2116"
+                        @click="validate"
+                      >
+                        Fund it !
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row align="center" class="mt-12">
+      <v-col align="center">
+        <v-card style="border-radius:30px ;width:65%"
+          ><h2
+            style="padding-top: 30px ; padding-left: 30px; font-size:22px"
+            class="toptext"
           >
-            <v-tabs
-              height="70px"
-              v-model="tab"
-              background-color="#977C1E"
-              class="elevation-2"
-              dark
-              :centered="centered"
-              :grow="grow"
-              :vertical="vertical"
-              :right="right"
-              :prev-icon="
-                prevIcon ? 'mdi-arrow-left-bold-box-outline' : undefined
-              "
-              :next-icon="
-                nextIcon ? 'mdi-arrow-right-bold-box-outline' : undefined
-              "
+            We are developing a website that helps hotels reach more clients
+            through virtual visits.It will revolutionize the tourism approach in
+            Tunisia.It must be done by the end of august ...
+          </h2>
+          <v-row style="padding-top: 10px "
+            ><v-col align="center">
+              <v-btn
+                style="color: white"
+                rounded
+                width="180px"
+                class="btn black"
+              >
+                Read More
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col></v-row
+    >
+    <v-row align="center" class="mt-12 pt-12">
+      ><v-col align="center">
+        <v-tabs
+          height="70px"
+          v-model="tab"
+          :centered="true"
+          :grow="grow"
+          :vertical="vertical"
+          :right="right"
+          :prev-icon="prevIcon ? 'mdi-arrow-left-bold-box-outline' : undefined"
+          :next-icon="nextIcon ? 'mdi-arrow-right-bold-box-outline' : undefined"
+        >
+          <v-tabs-slider></v-tabs-slider>
+          <v-tab :key="4" :href="`#tab-${4}`" style="width: 200px">
+            <i class="fa fa-heart" aria-hidden="true" style="width: 200px"></i>
+          </v-tab>
+          <v-tab :key="1" :href="`#tab-${1}`" style="width: 200px">
+            <i
+              class="fa fa-comment"
+              aria-hidden="true"
+              style="padding-right: 10px"
+            ></i>
+            Commentaires
+          </v-tab>
+          <v-tab :key="2" :href="`#tab-${2}`" style="width: 200px">
+            <i
+              class="fa fa-code"
+              aria-hidden="true"
+              style="padding-right: 10px"
+            ></i>
+            FAQ
+          </v-tab>
+          <v-tab :key="3" :href="`#tab-${3}`" style="width: 200px">
+            <i
+              class="fa fa-database"
+              aria-hidden="true"
+              style="padding-right: 10px"
+            ></i>
+            Statistics
+          </v-tab>
+
+          <v-tab-item :key="1" :value="'tab-' + 1">
+            <v-card
+              height="400px"
+              style=" padding-top: 30px;padding-left: 190px"
             >
-              <v-tabs-slider color="#012116"></v-tabs-slider>
-              <v-tab :key="4" :href="`#tab-${4}`" style="width: 200px">
-                <i
-                  class="fa fa-heart"
-                  aria-hidden="true"
-                  style="width: 200px"
-                ></i>
-              </v-tab>
-              <v-tab :key="1" :href="`#tab-${1}`" style="width: 200px">
-                <i
-                  class="fa fa-comment"
-                  aria-hidden="true"
-                  style="padding-right: 10px"
-                ></i>
-                Commentaires
-              </v-tab>
-              <v-tab :key="2" :href="`#tab-${2}`" style="width: 200px">
-                <i
-                  class="fa fa-code"
-                  aria-hidden="true"
-                  style="padding-right: 10px"
-                ></i>
-                FAQ
-              </v-tab>
-              <v-tab :key="3" :href="`#tab-${3}`" style="width: 200px">
-                <i
-                  class="fa fa-database"
-                  aria-hidden="true"
-                  style="padding-right: 10px"
-                ></i>
-                Statistics
-              </v-tab>
+              <v-toolbar height="90px" width="700px">
+                
+                 <v-list-item-avatar>
+                  <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
+                  </v-list-item-avatar>
+            <v-divider></v-divider>
+            
+                <v-toolbar-title style="padding-right: 20px  "
+                  >" I believe that this project has a great future that's why i
+                  invested in it "
+                </v-toolbar-title>
+              </v-toolbar>
+              <br />
 
-              <v-tab-item :key="1" :value="'tab-' + 1">
-                <v-card
-                  height="400px"
-                  style=" background-color: #012116; padding-top: 30px;padding-left: 190px"
-                >
-                  <v-toolbar color="#977C1E" height="90px" width="700px">
-                    <v-toolbar-title style="padding-right: 20px  "
-                      >" I believe that this project has a great future that's
-                      why i invested in it "
+              <v-row>
+                <v-col>
+                  <v-btn style="padding-right: 20px   ; background-color:gray; "
+                    ><i
+                      class="fa fa-comment"
+                      aria-hidden="true"
+                      style="padding-right: 10px"
+                    ></i
+                    >Reply on this comment ...
+                  </v-btn>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-btn style="padding-right: 20px ; background-color:gray; "
+                    ><i
+                      class="fa fa-comment"
+                      aria-hidden="true"
+                      style="padding-right: 10px"
+                    ></i
+                    >Add feedback ...
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item :key="4" :value="'tab-' + 4">
+            <v-card height="430px" 
+              ><v-row style="padding-top: 50px"
+                ><v-col>
+                  <v-card>
+                    <v-toolbar-title
+                      style="padding-right: 20px  "
+                      class="toptext"
+                      ><i class="fa fa-eye" aria-hidden="true"></i>120 views
                     </v-toolbar-title>
-                  </v-toolbar>
-                  <br />
-
-                  <v-toolbar
-                    color="#977C1E"
-                    height="50px"
-                    width="300px"
-                    style="margin-left: 400px"
-                  >
-                    <v-toolbar-title style="padding-right: 20px   "
+                  </v-card></v-col
+                >
+                <v-col>
+                  <v-card>
+                    <v-toolbar-title
+                      style="padding-right: 20px  "
+                      class="toptext"
                       ><i
                         class="fa fa-comment"
                         aria-hidden="true"
                         style="padding-right: 10px"
                       ></i
-                      >Reply on this comment ...
+                      >50 likes
                     </v-toolbar-title>
-                  </v-toolbar>
-                  <br />
-                  <v-toolbar color="#977C1E" height="90px" width="700px">
-                    <v-toolbar-title style="padding-right: 20px  "
-                      ><i
-                        class="fa fa-comment"
-                        aria-hidden="true"
-                        style="padding-right: 10px"
-                      ></i
-                      >Add feedback ...
-                    </v-toolbar-title>
-                  </v-toolbar>
-                </v-card>
-              </v-tab-item>
-              <v-tab-item :key="4" :value="'tab-' + 4">
-                <v-card
-                  height="430px"
-                  style=" background-color: #012116; padding-top: 30px;padding-left: 190px"
-                  ><v-row style="padding-top: 50px"
-                    ><v-col>
-                      <v-toolbar color="#977C1E" height="90px" width="170px">
-                        <v-toolbar-title style="padding-right: 20px  "
-                          ><i class="fa fa-eye" aria-hidden="true"></i>120 views
-                        </v-toolbar-title>
-                      </v-toolbar></v-col
-                    >
-                    <v-col>
-                      <v-toolbar color="#977C1E" height="90px" width="170px">
-                        <v-toolbar-title style="padding-right: 20px  "
-                          ><i
-                            class="fa fa-comment"
-                            aria-hidden="true"
-                            style="padding-right: 10px"
-                          ></i
-                          >50 likes
-                        </v-toolbar-title>
-                      </v-toolbar></v-col
-                    ></v-row
-                  >
-                </v-card>
-              </v-tab-item>
-              <v-tab-item :key="2" :value="'tab-' + 2">
-                <v-card
-                  height="400px"
-                  style=" background-color: #012116; padding-top: 30px;padding-left: 190px"
-                >
-                  <v-toolbar color="#977C1E" height="90px" width="700px">
-                    <v-toolbar-title style="padding-right: 20px  "
+                  </v-card></v-col
+                ></v-row
+              >
+            </v-card>
+          </v-tab-item>
+          <v-tab-item :key="2" :value="'tab-' + 2">
+            <v-card height="400px" >
+              <v-row align="center" justify="center">
+                <v-col align="center" justify="center" cols="4">
+                  <v-card cla>
+                    <v-toolbar-title style="padding-right: 20px  " class="toptext"
                       >" What's the purpose of this project ? "
                     </v-toolbar-title>
-                  </v-toolbar>
-                  <br />
+                  </v-card>
+                </v-col>
+              </v-row>
 
-                  <v-toolbar
-                    color="#977C1E"
-                    height="50px"
-                    width="300px"
-                    style="margin-left: 400px"
-                  >
-                    <v-toolbar-title style="padding-right: 20px   "
-                      ><i
-                        class="fa fa-comment"
-                        aria-hidden="true"
-                        style="padding-right: 10px"
-                      ></i
-                      >Reply to this question ...
-                    </v-toolbar-title>
-                  </v-toolbar>
-                  <br />
-                  <v-toolbar color="#977C1E" height="90px" width="700px">
-                    <v-toolbar-title style="padding-right: 20px  "
-                      ><i
-                        class="fa fa-comment"
-                        aria-hidden="true"
-                        style="padding-right: 10px"
-                      ></i
-                      >Add a question ...
-                    </v-toolbar-title>
-                  </v-toolbar>
-                </v-card>
-              </v-tab-item>
-              <v-tab-item :key="3" :value="'tab-' + 3">
-                <v-card
-                  height="400px"
-                  style=" background-color: #012116; padding-top: 30px;padding-left: 190px"
-                >
-                </v-card>
-              </v-tab-item>
-            </v-tabs> </v-card></v-hover></v-col
+              <v-row>
+                <v-col>
+                  <v-btn style="padding-right: 20px   ; background-color:gray; "
+                    ><i
+                      class="fa fa-comment"
+                      aria-hidden="true"
+                      style="padding-right: 10px"
+                    ></i
+                    >Reply ...
+                  </v-btn>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-btn style="padding-right: 20px ; background-color:gray; "
+                    ><i
+                      class="fa fa-comment"
+                      aria-hidden="true"
+                      style="padding-right: 10px"
+                    ></i
+                    >Add a question ...
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item :key="3" :value="'tab-' + 3">
+            <v-card
+              height="400px"
+              style=" background-color: #012116; padding-top: 30px;padding-left: 190px"
+            >
+            </v-card>
+          </v-tab-item> </v-tabs></v-col
     ></v-row></div
 ></template>
 
@@ -288,10 +265,24 @@ export default {
       prevIcon: false,
       nextIcon: false,
       right: false,
-      tabs: 4
+      tabs: 4,
     };
-  }
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.rounded {
+  border-radius: 15% !important;
+}
+.divider {
+  width: 60%;
+  border-top: 6px #0a2116 solid !important;
+}
+.toptext {
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 30px;
+}
+</style>
